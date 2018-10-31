97a60b0d82635905939787208ec6da8022db7737
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-31 20:07:22 +0300

    create public dir & add express.static

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 
 var app = express();
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+//let express use /public folder
+app.use(express.static(__dirname + /public));
+
+app.get('/', (req, res) => {
+  res.send({
+    name: 'Jassem',
+    works: [
+      'my site',
+      'my porfolio'
+    ]
+  });
+});
+
+app.listen(4000, () => {
+  console.log('Server is up on port 4000');
 });
