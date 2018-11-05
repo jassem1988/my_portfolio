8812191696421ed5a4dc6c5ddd386928965b4eac
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-05 10:26:03 +0300

    add comments to waypoints

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -18,9 +18,9 @@ $(document).ready(function() {
     $("html, body").animate({scrollTop: $(".js--header").offset().top}, 1000);
   });
 
-  // Navigation Scroll 
+  // Navigation Scroll
   $('a[href*=\\#]:not([href=\\#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
         || location.hostname == this.hostname) {
 
         var target = $(this.hash);
@@ -34,42 +34,40 @@ $(document).ready(function() {
     }
   });
 
-  // Animations on scroll 
+  // Animations on scroll
   $(".js--wp-1").waypoint(function(direction) {
-    $(".js--wp-1").addClass("animated fadeInLeft");
+    $(".js--wp-1").addClass("animated fadeInLeft"); //designer skills bars
   }, {
     offset: "70%"
   });
 
   $(".js--wp-2").waypoint(function(direction) {
-    $(".js--wp-2").addClass("animated fadeInRight");
+    $(".js--wp-2").addClass("animated fadeInRight"); //programmer skills bars
   }, {
     offset: "70%"
   });
 
-  $(".js--wp-3").waypoint(function(direction) {
-    $(".js--wp-3").addClass("animated fadeInUpBig slow");
-  }, {
-    offset: "40%"
-  });
+  // $(".js--wp-3").waypoint(function(direction) {
+  //   $(".js--wp-3").addClass("animated fadeInUpBig slow"); //not in use
+  // }, {
+  //   offset: "40%"
+  // });
 
   $(".js--wp-4").waypoint(function(direction) {
-    $(".js--wp-4").addClass("animated fadeInUp");
+    $(".js--wp-4").addClass("animated fadeInUp"); //h2 header
   }, {
     offset: "60%"
   });
 
   $(".js--wp-5").waypoint(function(direction) {
-    $(".js--wp-5").addClass("animated fadeInUp");
+    $(".js--wp-5").addClass("animated fadeInUp"); //Skills section
   }, {
     offset: "70%"
   });
 
   $(".js--wp-6").waypoint(function(direction) {
-    $(".js--wp-6").addClass("animated fadeInUp");
+    $(".js--wp-6").addClass("animated fadeInUp"); //Projects section
   }, {
     offset: "70%"
   });
-
-
 });
